9d65095282ded0ba3cb833545e292c881f6a90e9
Edward Lacerda <elacerda28@gmail.com>
2024-09-18 01:57:17 -0300

    commit projeto spring MVC

---
+++ src/test/java/com/project/DevHotel/DevHotelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.DevHotel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevHotelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
